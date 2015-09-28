5fe9909a543d6acf090a4601c8c4ccdf499aedd1
Aukraiser <quifouma.samuel@live.fr>
2015-09-28 23:38:30 +0200

    Entities modifications
    Added getters/setters generation

--- src/Sam/Project/ProcatBundle/Entity/Category.php
+++ src/Sam/Project/ProcatBundle/Entity/Category.php
@@ -4,6 +4,8 @@ namespace Sam\Project\ProcatBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
+use Sam\Project\ProcatBundle\Entity\Product;
+use Sam\Project\ProcatBundle\Entity\Category;
 
 /**
  * Category
@@ -69,5 +71,144 @@ class Category
     {
         return $this->id;
     }
-}
 
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set level
+     *
+     * @param integer $level
+     *
+     * @return Category
+     */
+    public function setLevel($level)
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    /**
+     * Get level
+     *
+     * @return integer
+     */
+    public function getLevel()
+    {
+        return $this->level;
+    }
+
+    /**
+     * Add product
+     *
+     * @param \Sam\Project\ProcatBundle\Entity\Product $product
+     *
+     * @return Category
+     */
+    public function addProduct(Product $product)
+    {
+        $this->products[] = $product;
+
+        return $this;
+    }
+
+    /**
+     * Remove product
+     *
+     * @param \Sam\Project\ProcatBundle\Entity\Product $product
+     */
+    public function removeProduct(Product $product)
+    {
+        $this->products->removeElement($product);
+    }
+
+    /**
+     * Get products
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getProducts()
+    {
+        return $this->products;
+    }
+
+    /**
+     * Add child
+     *
+     * @param \Sam\Project\ProcatBundle\Entity\Category $child
+     *
+     * @return Category
+     */
+    public function addChild(Category $child)
+    {
+        $this->children[] = $child;
+
+        return $this;
+    }
+
+    /**
+     * Remove child
+     *
+     * @param \Sam\Project\ProcatBundle\Entity\Category $child
+     */
+    public function removeChild(Category $child)
+    {
+        $this->children->removeElement($child);
+    }
+
+    /**
+     * Get children
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getChildren()
+    {
+        return $this->children;
+    }
+
+    /**
+     * Set parent
+     *
+     * @param \Sam\Project\ProcatBundle\Entity\Category $parent
+     *
+     * @return Category
+     */
+    public function setParent(Category $parent = null)
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * Get parent
+     *
+     * @return \Sam\Project\ProcatBundle\Entity\Category
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+}
--- src/Sam/Project/ProcatBundle/Entity/Product.php
+++ src/Sam/Project/ProcatBundle/Entity/Product.php
@@ -3,6 +3,7 @@
 namespace Sam\Project\ProcatBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Sam\Project\ProcatBundle\Entity\Category;
 
 /**
  * Product
@@ -51,5 +52,76 @@ class Product
     {
         return $this->id;
     }
-}
 
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set likes
+     *
+     * @param integer $likes
+     *
+     * @return Product
+     */
+    public function setLikes($likes)
+    {
+        $this->likes = $likes;
+
+        return $this;
+    }
+
+    /**
+     * Get likes
+     *
+     * @return integer
+     */
+    public function getLikes()
+    {
+        return $this->likes;
+    }
+
+    /**
+     * Set category
+     *
+     * @param \Sam\Project\ProcatBundle\Entity\Category $category
+     *
+     * @return Product
+     */
+    public function setCategory(Category $category = null)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return \Sam\Project\ProcatBundle\Entity\Category
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+}
