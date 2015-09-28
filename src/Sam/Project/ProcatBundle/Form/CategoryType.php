ec48670dc2a174081a919dcccc7209a8b4c83774
Aukraiser <quifouma.samuel@live.fr>
2015-09-28 23:55:08 +0200

    Forms generation
    Generated forms for the entities

---
+++ src/Sam/Project/ProcatBundle/Form/CategoryType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Sam\Project\ProcatBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CategoryType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('level')
+            ->add('parent')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Sam\Project\ProcatBundle\Entity\Category'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'sam_project_procatbundle_category';
+    }
+}
---
+++ src/Sam/Project/ProcatBundle/Form/ProductType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Sam\Project\ProcatBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ProductType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('likes')
+            ->add('category')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Sam\Project\ProcatBundle\Entity\Product'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'sam_project_procatbundle_product';
+    }
+}
